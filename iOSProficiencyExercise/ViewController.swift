00104665c26d85fe5dc22ad064953a3733bce360
Liu Changhong <liuchanghong@Lius-MacBook-Pro.local>
2020-04-05 20:37:19 +1000

    Project Init

---
+++ iOSProficiencyExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSProficiencyExercise
+//
+//  Created by Liu Changhong on 5/4/20.
+//  Copyright © 2020 Liu Changhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
